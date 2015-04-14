7f7575ff1ea6c04dac74232bf2bd772b9ea1c47d
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-04-14 15:09:15 +0200

    deal with rename, tests

--- src/superagent.js
+++ src/superagent.js
@@ -1,4 +1,4 @@
-import {Request} from 'oauth2-client';
+import {Request} from 'oauth2-client-js';
 
 export default function(superagent) {
     /**
@@ -17,20 +17,34 @@ export default function(superagent) {
         });
     }
 
-    superagent.Request.prototype.exec = function(requestConfig, applyMetadataFn) {
+    superagent.Request.prototype.exec = function(applyMetadataFn) {
         // if this request doesn't have oauth enabled,
         // just execute itgw
         if (!this._oauthEnabled) {
             return exec.call(this);
         }
         return new Promise((resolve, reject) => {
+
+            function requestAccessToken() {
+                // no token. we need to request one
+                let authRequest = new Request(this._oauthRequestConfig);
+                if (applyMetadataFn) {
+                    applyMetadataFn(authRequest);
+                }
+                let redirectionUri = this._oauthProvider.requestToken(authRequest);
+                // remember this request for later when we get a response
+                this._oauthProvider.remember(authRequest);
+                // request is done via redirect to auth provider
+                reject(authRequest);
+                if (ENV_PRODUCTION) {
+                    window.location.href = redirectionUri;
+                }
+            }
+
             let provider = this._oauthProvider;
             // otherwise we need to
             // - check if we have a token for this provider in our storage
             if (provider.hasAccessToken()) {
-                if (!ENV_PRODUCTION) {
-                    console.log('Provider has access token, will use it.');
-                }
                 // there is a token and WE WILL USE IT FOR FUCKS SAKE
                 // it might be invalid tho
 
@@ -40,19 +54,39 @@ export default function(superagent) {
                 exec
                     .call(this)
                     .then(resolve)  // token was apparently ok
-                    .catch();   //FIXME check the error and update token if necessary
+                    .catch(err => {
+                        if (err.status === 401 ) {
+                            // Unauthorized
+                            
+                            if (provider.hasRefreshToken()) {
+                                superagent
+                                    .get(provider.refreshToken())
+                                    .exec()
+                                    .then(resp => {
+                                        try {
+                                            provider.handleResponse(resp);
+                                            this
+                                                .exec(applyMetadataFn)
+                                                .then(resolve)
+                                                .catch(e => reject(e));
+                                        } catch(handleResponseError) {
+                                            reject(handleResponseError);
+                                        }
+                                        
+                                    })
+                                    .catch(reject);
+                            } else {
+                                // No refresh token, we need to request a new access token
+                                requestAccessToken.call(this);
+                            }
+                        } else {
+                            // We got an error, but the token appears to be valid
+                            // reject and pass the error
+                            reject(err);
+                        }
+                    });
             } else {
-                // no token. we need to request one
-                let authRequest = new Request(requestConfig);
-                authRequest.metadata = applyMetadataFn ? applyMetadataFn() : undefined;
-                let redirectionUri = provider.requestToken(authRequest);
-                // remember this request for later when we get a response
-                provider.remember(authRequest);
-                // request is done via redirect to auth provider
-                reject(authRequest);
-                if (ENV_PRODUCTION) {
-                    window.location.href = redirectionUri;
-                }
+                requestAccessToken.call(this);
             }
         });
     };
@@ -63,9 +97,10 @@ export default function(superagent) {
      * @param  {OAuthProvider} provider
      * @return {self} superagent
      */
-    superagent.Request.prototype.oauth = function(provider) {
+    superagent.Request.prototype.oauth = function(provider, requestConfig) {
         this._oauthEnabled = true;
         this._oauthProvider = provider;
+        this._oauthRequestConfig = requestConfig;
         return this;
     };
 
--- test/superagent.test.js
+++ test/superagent.test.js
@@ -1,14 +1,14 @@
 import superagent from 'superagent';
 import patchSuperagent from '../src/superagent';
-import {Provider, MemoryStorage} from 'oauth2-client';
+import {Provider, MemoryStorage} from 'oauth2-client-js';
 
-describe('superagent', () => {
+describe('superagent-oauth2-client', () => {
     var request, provider;
     
     beforeEach(() => {
         provider = new Provider({
             id: 'test',
-            authorization_url: 'test',
+            authorization_url: 'localhost/test',
             store: new MemoryStorage()
         });
         request = patchSuperagent(superagent);
@@ -16,32 +16,6 @@ describe('superagent', () => {
 
     afterEach(() => {
         request = null;
-    });
-
-    // it('#exec() should return a promise', () => {
-    //     let req = request
-    //                 .get('some.whe.re')
-    //                 .exec();
-    //     expect(req instanceof Promise).to.be.true;
-    // });
-
-    it('#exec() should apply and save metadata', (done) => {
-        let req = request
-                    .get('buuh')
-                    .oauth(provider)
-                    .exec({
-                        client_id: '123',
-                        redirect_uri: 'localhost'
-                    }, () => ({
-                        timestamp: 123
-                    }));
-        expect(req instanceof Promise).to.be.true;
-        req.catch(req => {
-            expect(req.metadata.timestamp).to.equal(123);
-            expect(provider.store.get(req.state).metadata).to.be.ok;
-            expect(provider.store.get(req.state).metadata.timestamp).to.equal(123);
-            done();
-        });
     });
 
     it('#oauth() should enable oauth', () => {
@@ -53,19 +27,145 @@ describe('superagent', () => {
         expect(req._oauthEnabled).to.be.true;
     });
 
-    // it('#oauth() should use an available access token', (done) => {        
-    //     provider.setAccessToken('token');
+    it('#exec() should return a promise', () => {
+        let req = request
+                    .get('some.whe.re')
+                    .exec();
+        expect(req instanceof Promise).to.be.true;
+    });
 
-    //     let req = request
-    //                 .get('testlocation')
-    //                 .oauth(provider);
+    it('#exec() should apply and save metadata', (done) => {
+        let req = request
+                    .get('buuh')
+                    .oauth(provider, {
+                        client_id: '123',
+                        redirect_uri: 'localhost'
+                    })
+                    .exec(req => req.metadata.timestamp = 123);
+        expect(req instanceof Promise).to.be.true;
+        req.catch(req => {
+            expect(req.metadata.timestamp).to.equal(123);
+            expect(provider.store.get(req.state).metadata).to.be.ok;
+            expect(provider.store.get(req.state).metadata.timestamp).to.equal(123);
+            done();
+        });
+    });
 
-    //     req.end = function() {
-    //         //TODO this is quite implementation-specific ;_;
-    //         expect( this._header.authorization ).to.equal('Token token');
-    //         done();
-    //     };
 
-    //     req.exec();
-    // });
+    it('#exec() should use an available access token', (done) => {        
+        provider.setAccessToken('token');
+
+        nock('testlocation').get('/').reply(200);
+
+        let req = request
+                    .get('testlocation')
+                    .oauth(provider, {
+                        client_id: 'client_id',
+                        redirect_uri: 'localhost'
+                    });
+
+        req.end = function() {
+            //TODO this is quite implementation-specific ;_;
+            console.log(this._header);
+            expect( this._header.authorization ).to.equal('Token token');
+            done();
+        };
+
+        req.exec();
+    });
+
+    it('#exec() should reject if it encounters an error other than 401', done => {
+        provider.setAccessToken('token');
+        var req = request
+                    .get('testlocation')
+                    .oauth(provider, {
+                        client_id: 'client',
+                        redirect_uri: 'localhost'
+                    });
+
+        // mock error server response
+        req.end = function(cb) {
+            let errorResp = {
+                status: 503
+            };
+            cb(errorResp);
+        };
+
+        req.exec().catch(() => done());
+    });
+
+    it('#exec() should issue a proper auth request', done => {
+        var req = request
+                    .get('testlocation')
+                    .oauth(provider, {
+                        client_id: 'client_id',
+                        redirect_uri: 'localhost'
+                    });
+
+        req.exec().catch(auth => {
+            expect(auth.client_id).to.equal('client_id');
+            expect(auth.redirect_uri).to.equal('localhost');
+            expect(auth.state).to.be.ok;
+            done();
+        });
+    });
+
+    it('#exec() should issue an auth request if there is no refresh token', done => {
+        var req = request
+                    .get('testlocation')
+                    .oauth(provider, {
+                        client_id: 'client_id',
+                        redirect_uri: 'localhost'
+                    });
+        provider.setAccessToken('access_token');
+
+        req.end = function(cb) {
+            cb({
+                status: 401,
+                body: {
+                    error: 'invalid_request'
+                }
+            });
+        };
+
+        req.exec().catch(auth => {
+            expect(auth.client_id).to.equal('client_id');
+            expect(auth.redirect_uri).to.equal('localhost');
+            expect(auth.state).to.be.ok;
+            done();
+        });
+    });
+
+    it('#exec() should request a new access token with an available refresh token', done => {
+        var req = request
+                    .get('testlocation')
+                    .oauth(provider, {
+                        client_id: 'client_id',
+                        redirect_uri: 'localhost'
+                    });
+        provider.setAccessToken('access_token');
+        provider.setRefreshToken('refresh_token');
+
+        var _exec = req.exec;
+        req.exec = function exec() {
+            return _exec.call(this);
+        };
+
+        req.end = function(cb) {
+            cb({
+                status: 401,
+                body: {
+                    error: 'invalid_request'
+                }
+            });
+        };
+
+        req.exec().catch(err => {
+            let {error} = err.response;
+            expect(error.url.indexOf('grant_type=refresh_token') >= 0).to.be.true;
+            expect(error.url.indexOf('refresh_token=refresh_token') >= 0).to.be.true;
+            done();
+        });
+
+    });
 });
